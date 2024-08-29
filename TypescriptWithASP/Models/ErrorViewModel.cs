7a8b22b9fed1615003bf1161572e73348a364b2f
lenarosmayani <rosmayanilena@gmail.com>
2024-08-29 17:13:38 +0700

    first commit

---
+++ TypescriptWithASP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TypescriptWithASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
